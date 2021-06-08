5634013584b09b05641de4f700d95723ed1e80c3
NicoRosero <nicolas.nacho93@gmail.com>
2021-06-07 22:22:42 -0500

    updated car
    removed monobehavior father class for not using it

--- OOPreview/Assets/Scripts/Car.cs
+++ OOPreview/Assets/Scripts/Car.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Car : MonoBehaviour
+public class Car
 {
     public string make = "FIAT";
     public string model = "500";
@@ -11,33 +11,7 @@ public class Car : MonoBehaviour
     public int hp = 150;
     private int numberOfPistons = 3;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("Start");
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-        if (numberOfPistons > 3)
-        {
-
-        }
-
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            Debug.Log("About to start my " + " " + make + " " + model);
-            StartCar();
-        }
-
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Debug.Log("About to stop my " + " " + make + " " + model);
-            StopCar();
-        }
-    }
     void StartCar()
     {
         Debug.Log("Brrrrrrrrrrrrrrummmmmmmmmmm");
