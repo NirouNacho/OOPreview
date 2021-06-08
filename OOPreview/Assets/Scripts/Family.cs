578b2bdad5f2ba90cb127dc382f98d3038f920e9
NicoRosero <nicolas.nacho93@gmail.com>
2021-06-07 22:50:57 -0500

    added family class
    for familiy / person object

---
+++ OOPreview/Assets/Scripts/Family.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Family : MonoBehaviour
+{
+    public Person father;
+    public Person mother;
+    public Person son;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        father = new Person();
+        father.name = "Patricio";
+        father.lastName = "Rosero";
+        father.secondLastName = "Jacome";
+        father.age = 67;
+        father.isMarried = true;
+
+
+        mother = new Person();
+        mother.name = "Praxedes";
+        mother.lastName = "Urrutia";
+        mother.secondLastName = "Sepulveda";
+        mother.age = 64;
+        mother.isFather = false;
+        mother.isMarried = true;
+
+        son = new Person();
+        son.name = "Nicolas";
+        son.lastName = father.lastName;
+        son.secondLastName = mother.lastName;
+        son.age = 25;
+        son.isFather = false;
+        son.isMarried = false;
+
+
+        print(father.name + "and" + mother.name + "have a son called :"
+            + son.name + "who's" + son.age + " years old."
+            );
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ OOPreview/Assets/Scripts/Person.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person 
+{
+    public string name = "";
+    public string lastName = "";
+    public string secondLastName = "";
+    public int age = 0;
+    public string address = "";
+    public bool isMarried = false;
+    public Car car;
+
+}
